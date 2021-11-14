ae04bca6e37bfc2b64ebfb44b8ac6ff99019b58a
Frank <Tinsley>
2021-11-14 13:44:11 -0800

    Player has a score that begins at 0

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int playerScore = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
